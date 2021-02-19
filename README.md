16664dd22a907ea3b3d7aefc79281080387abe27
royweb <xytank@gmail.com>
2021-02-19 12:54:10 -0500

    Set up create react app

---
+++ README.md
@@ -0,0 +1,1 @@
+# WRI Share Pic
---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WRI Share Pic</h1>
+    </div>
+  );
+}
+
+export default App;
