345dcb22685c1b6bf068a2acff1aec950e05476a
royweb <xytank@gmail.com>
2021-02-19 14:17:49 -0500

    Add logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="images/logo.png"
+          alt="Logo" />
+      </div>
       <h1>WRI Share Pic</h1>
     </div>
   );
