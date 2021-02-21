8b36997b4ec7cfc44bcdaa4706465116f3938399
royweb <xytank@gmail.com>
2021-02-21 13:23:20 -0500

    Refactor post component

--- src/App.js
+++ src/App.js
@@ -11,9 +11,18 @@ function App() {
           src="images/logo.png"
           alt="Logo" />
       </div>
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        imageUrl="https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c3R1ZHl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+        username="Ken Car"
+        caption="Keep up the good work!" />
+      <Post
+        imageUrl="https://images.unsplash.com/photo-1424847651672-bf20a4b0982b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8ZWF0aW5nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        username="Eating Time"
+        caption="I love my foods" />
+      <Post
+        imageUrl="https://images.unsplash.com/photo-1477332552946-cfb384aeaf1c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8cnVubmluZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        username="Jill Running"
+        caption="Walking at sun rise" />
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,24 +3,24 @@ import Avatar from "@material-ui/core/Avatar"
 
 import './Post.css'
 
-function Post() {
+function Post({ imageUrl, username, caption }) {
     return (
         <div className="post">
             <div className="post__header">
                 <Avatar
                     className="post__avatar"
-                    alt="Ken Car"
+                    alt={username}
                     src="1" />
-                <h3>Ken Car</h3>
+                <h3>{username}</h3>
             </div>
 
             <img
                 className="post__image"
-                src="https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c3R1ZHl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+                src={imageUrl}
                 alt="Post" />
             
             <h4 className="post__text">
-                <strong>Bob: </strong>Keep up the good work!
+                <strong>{username}: </strong>{caption}
             </h4>
         </div>
     )
